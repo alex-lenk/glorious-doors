697f26731ddefc30938c834546b1314f1928778d
artlenk.ru@gmail.com <df5ukoi7ht>
2017-05-26 16:51:03 +0300

    Cleaned common.js from comments

--- src/js/common.js
+++ src/js/common.js
@@ -1,35 +1,3 @@
-/*
-$(".navbar-toggle").click(
-    function () {
-        $('.navbar-menu').toggleClass("navbar-menu__active");
-    }
-);
-
-(function ($) {
-    $(function () {
-
-        $('.blog-filter-select').styler();
-
-    });
-})(jQuery);
-
-$('.related-posts-carousel').slick({
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 5,
-    nextArrow: '<span class="slick-next slick-arrow icon-next"></span>',
-    prevArrow: '<span class="slick-prev slick-arrow icon-prev"></span>',
-    responsive: [
-        {
-            breakpoint: 959,
-            settings: {
-                slidesToShow: 3
-            }
-        }
-    ]
-});
-*/
 $(".mobile-menu").click(
     function () {
         $('.top-nav-menu').toggleClass("top-nav-menu__active");
